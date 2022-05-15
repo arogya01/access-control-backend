9aec12b56ceae9008ffdadcf2b3299b6d9101a44
arogya <arogyabichpuria16@gmail.com>
2022-05-15 12:57:00 +0530

    changed a typo

--- middleware/ensureLogin.js
+++ middleware/ensureLogin.js
@@ -1,5 +1,5 @@
-require('dotenv').config();
-const userController=require("../controllers/users");
+require("dotenv").config();
+const userController = require("../controllers/users");
 const express = require("express");
 const app = express();
 app.use(express.json());
@@ -7,19 +7,18 @@ app.use(express.json());
 // this middleware will run through to ensure that the user who is accessing this route is authorized to do so
 const jwt = require("jsonwebtoken");
 
-module.exports = function ensureLogin(req,res,next){
-    const jwt_token = req.header("authorization");
-   
-    jwt.verify(jwt_token,process.env.SECRET_KEY_ACCESS,(err,payload)=>{
-        if(err){
-            return res.status(401).json({error:"you must be logged in"});
-        }
+module.exports = function ensureLogin(req, res, next) {
+  const jwt_token = req.header("Authorization");
+  console.log(jwt_token);
+  jwt.verify(jwt_token, process.env.SECRET_KEY_ACCESS, (err, payload) => {
+    if (err) {
+      return res.status(401).json({ error: "you must be logged in" });
+    }
 
-        const {email} = payload;
+    const { email } = payload;
 
-        const result =userController.findUser({email:email});
-        req.user={result};
-        next();
-    })
-    
-}
+    const result = userController.findUser({ email: email });
+    req.user = { result };
+    next();
+  });
+};
